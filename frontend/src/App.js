adc2ee0c9603c4637125fa80c0512c91eb338373
Artem <v.amuzinsky@yandex.ru>
2024-10-25 17:12:23 +0300

    весь фронт? no

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Body } from './components/Body';
+import { Reg } from './components/Reg';
+import './css/page.css';
+import './css/reg.css';
+
+
+function App() {
+    const [showReg, setShowReg] = useState(false);
+
+    const handleRegisterClick = () => {
+        setShowReg(true);
+    };
+
+    return (
+        <div className="App">
+            <Header />
+            {showReg ? <div><Body/></div> :
+                <div className="registration-container">
+                    <Reg/>
+                    <button type="submit" className="submit">Зарегистрироваться</button>
+                    <button onClick={handleRegisterClick} className="submit">Войти</button>
+                </div>}
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
